9b256b16a9ef090237144f3ea64a2fd727b6e5ca
Zann <damiryusupov2504@gmail.com>
2023-10-31 17:06:41 +0500

    new1

---
+++ prjkn/Form1.cs
@@ -0,0 +1,15 @@
+namespace prjkn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            panel1.BackColor = Color.FromArgb(100, 100, 100, 100);
+        }
+    }
+}
